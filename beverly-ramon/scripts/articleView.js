c899cd1b4bdb457d637e805307b3c71d197a1e94
brickfaced <brickfazed@hotmail.com>
2018-02-15 11:10:37 -0800

    Finished teaser code that only reveals the article that is clicked on

--- beverly-ramon/scripts/articleView.js
+++ beverly-ramon/scripts/articleView.js
@@ -70,7 +70,7 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
   $('.tab').on('click', function() {
@@ -85,14 +85,16 @@ articleView.handleMainNav = function() {
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
-  $((this) ,'.read-on').on('click', function() {
-
+  $('.read-on').on('click', function(event) {
+    event.preventDefault;
+    $(this).parent().find('*').show();
+    $(this).hide();
   })
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   $('article.template').hide();
   articleView.populateFilters();
